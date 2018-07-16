5377fd4e2daebd093e5b2213dbea1d2664003e95
Danielwhyte <danwhy@gmail.com>
2018-07-16 16:35:17 +0100

    lambda for uploading files to s3, #3

---
+++ lambda/uploadFile/index.js
@@ -0,0 +1,21 @@
+var multipart = require('parse-multipart');
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3();
+
+exports.handler = function(event, context, callback) {
+  var bodyBuffer = new Buffer(event['body-json'].toString(), 'base64');
+  var boundary = multipart.getBoundary(event.params.header['content-type']);
+
+  var parts = multipart.Parse(bodyBuffer, boundary);
+
+  return s3.putObject({
+    Bucket: '360-lm-test',
+    Key: 'tmp.zip',
+    Body: parts[0].data,
+    ACL: 'public-read'
+  }, function (err, resp) {
+    if (err) return callback(err);
+    console.log('Successfully uploaded');
+    return callback(null, {result: 'SUCCESS', files: parts});
+  });
+}
